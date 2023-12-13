17532fb9e4d55227f6e7e21b0fa8ad6d56b20b31
AzusaFoT <fot531@gmail.com>
2023-12-13 13:24:09 +0800

    <merge>

--- test.js
+++ test.js
@@ -1,4 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
+    //隨機字串區
+    function generateRandomString() {
+        // 取得當天時間的基數
+        const baseNumber = new Date().getTime();
+
+        // 生成前4個字母
+        const firstFourLetters = baseNumber
+            .toString(36)
+            .slice(-5)
+            .toUpperCase();
+
+        // 生成後2個字母
+        const randomLetters = generateRandomLetters(2);
+
+        // 組合字串
+        const randomString = firstFourLetters + randomLetters;
+
+        return randomString;
+    }
+
+    function generateRandomLetters(length) {
+        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let result = "";
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            result += characters.charAt(randomIndex);
+        }
+        return result;
+    }
+
+    // 生成隨機字串
+    const randomString = generateRandomString();
+
     function delay(ms) {
         return new Promise((resolve) => setTimeout(resolve, ms));
     }
@@ -11,16 +44,16 @@ document.addEventListener("DOMContentLoaded", function () {
     let canpress = false;
     let sessionState = 0;
     // 產生go和nogo的序列
-    const trial_num = 10;
+    //const trial_num = 10;
 
     function generateRandomArray(mode) {
         let length = 0;
         let counts = {};
 
         if (mode === 1) {
-            counts = { 0: 5, 1: 5 };
+            counts = { 0: 1, 1: 1 };
         } else if (mode === 2) {
-            counts = { 0: 4, 1: 4, 2: 1, 3: 1 };
+            counts = { 0: 1, 1: 1, 2: 1, 3: 1 };
         } else {
             throw new Error("Invalid mode");
         }
@@ -73,13 +106,15 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("resize", updateCanvasPosition);
 
     /* 按鈕載入現場 */
+    const circleRadius = Math.sqrt((100 * 100) / Math.PI);
+
     const confirmButton_L = new PIXI.Graphics();
     confirmButton_L.lineStyle(0); // draw a circle, set the lineStyle to zero so the circle doesn't have an outline
     confirmButton_L.beginFill(0xde3249, 1);
     confirmButton_L.drawCircle(
         app.screen.width / 6,
         (app.screen.height / 7) * 6,
-        50
+        circleRadius
     );
     confirmButton_L.endFill();
     confirmButton_L.interactive = true;
@@ -96,6 +131,50 @@ document.addEventListener("DOMContentLoaded", function () {
     confirmButton_R.endFill();
     confirmButton_R.interactive = true;
     confirmButton_R.buttonMode = true;
+
+    /* 複製按鈕在這 */
+    // 創建按鈕
+    async function show_copy_button() {
+        if (btn != null) {
+            app.stage.removeChild(btn);
+            app.stage.removeChildren();
+        }
+        // 當img資源載入完成時，創建img精靈
+        let texture_startButton = loader.resources.startButton.texture;
+        // texture_startButton = loader.resources.Button.texture;
+        show_frame();
+        btn = new PIXI.Sprite(texture_startButton);
+        await delay(1);
+        btn.interactive = true;
+        btn.buttonMode = true;
+        btn.anchor.set(0.5); // 設置錨點在中心
+        // 計算按鈕的寬度，使其等於背景物件寬度的 0.7 倍
+        const buttonWidth = background.width * 0.5;
+        const scale = buttonWidth / btn.width;
+        btn.scale.set(scale);
+        btn.position.set(app.screen.width / 2, (app.screen.height / 7) * 6); // 設置位置在舞台中下
+        app.stage.addChildAt(btn, 1); //上一層
+
+        btn.on("pointertap", async function () {
+            // 在這裡處理按鈕被點擊的邏輯
+            console.log(randomString);
+            copyToClipboard(randomString);
+        });
+    }
+    // 複製到剪貼簿的函數
+    function copyToClipboard(text) {
+        var textarea = document.createElement("textarea");
+        textarea.value = text;
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand("copy");
+        document.body.removeChild(textarea);
+        // 在這裡設置你想要打開的網頁的URL
+        //var url = "https://www.example.com";
+        var url = "https://forms.gle/cxHNT9hJKRneNb3w5";
+        // 使用 window.open 開啟新的網頁
+        window.open(url, "_blank");
+    }
 
     /* 按鈕載入現場 */
     function updateButton() {
@@ -164,7 +243,7 @@ document.addEventListener("DOMContentLoaded", function () {
     loader.add("instruction2", "resource/instruction2.png");
 
     /* frame */
-    loader.add("bg", "stimuli/frame.png");
+    loader.add("frame", "stimuli/frame.png");
 
     /* fixpoint */
     loader.add("fixpoint", "resource/fixpoint.json");
@@ -185,6 +264,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     /* 開始鈕 */
     loader.add("startButton", "stimuli/startButton.png");
+
+    /* 結束鈕 */
+    loader.add("endButton", "stimuli/endButton.png");
 
     /* 載入資源結束 */
 
@@ -227,23 +309,29 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     //開始//
-    function handleLoadComplete() {
+    async function handleLoadComplete() {
         const session1Array = generateRandomArray(1);
-        const session2Array = generateRandomArray(2).concat(
-            generateRandomArray(2)
-        );
-        startButton(session2Array);
-        //session(session2Array); //go
+        // console
+        await startButton(session1Array);
+        // show_bg();
+        // session(session2Array); //go
         // session(session2Array); //nogo
     }
     // 添加點擊監聽器
 
+    async function consoleAsync(str1) {
+        console.log(str1);
+    }
+
     async function startButton(sArray) {
         if (btn != null) {
             app.stage.removeChild(btn);
+            app.stage.removeChildren();
         }
         // 當img資源載入完成時，創建img精靈
         let texture_startButton = loader.resources.startButton.texture;
+        // texture_startButton = loader.resources.Button.texture;
+
         btn = new PIXI.Sprite(texture_startButton);
         await delay(1);
         btn.interactive = true;
@@ -265,7 +353,55 @@ document.addEventListener("DOMContentLoaded", function () {
             if (btn != null) {
                 app.stage.removeChild(btn);
             }
-            session(sArray); //go
+            await session(sArray); //go
+            sessionState = 2;
+            if (sessionState == 2) {
+                show_bg(2);
+                await consoleAsync("session1");
+                await show_secondButton();
+            }
+            //show_wrongkey();
+        });
+    }
+
+    async function show_secondButton() {
+        if (btn != null) {
+            app.stage.removeChild(btn);
+            app.stage.removeChildren();
+        }
+        show_bg(2);
+        // 當img資源載入完成時，創建img精靈
+        let texture_startButton = loader.resources.startButton.texture;
+        // texture_startButton = loader.resources.Button.texture;
+        btn = new PIXI.Sprite(texture_startButton);
+        await delay(1);
+        btn.interactive = true;
+        btn.buttonMode = true;
+        btn.anchor.set(0.5); // 設置錨點在中心
+        // 計算按鈕的寬度，使其等於背景物件寬度的 0.7 倍
+        const buttonWidth = background.width * 0.5;
+        const scale = buttonWidth / btn.width;
+        btn.scale.set(scale);
+        btn.position.set(app.screen.width / 2, (app.screen.height / 7) * 6); // 設置位置在舞台中下
+        app.stage.addChildAt(btn, 1); //上一層
+
+        btn.on("pointertap", async function () {
+            // 在這裡處理按鈕被點擊的邏輯
+            console.log("按鈕被點擊了！");
+            btn.interactive = false;
+            btn.buttonMode = false;
+            sessionState = 2;
+            if (btn != null) {
+                app.stage.removeChild(btn);
+            }
+            let sArray = generateRandomArray(2).concat(generateRandomArray(2));
+            await session(sArray); //go
+            sessionState = 3;
+            if (sessionState == 3) {
+                show_text();
+                show_copy_button();
+            }
+            //show_wrongkey();
         });
     }
 
@@ -331,48 +467,6 @@ document.addEventListener("DOMContentLoaded", function () {
             await delay(2000);
         }
     }
-    // async function gotrial(x) {
-    //     z = x;
-    //     await delay(500); // 等待500毫秒
-    //     show_fixpoint(); //播放
-    //     await delay(1000); //等待播放結束
-    //     canpress = true; //設定可以被按下
-    //     show_arrow(x); //播放
-    //     await delay(500); //等待播放結束
-    //     //如果被按下會是false，沒按則是true
-    //     if (canpress) {
-    //         canpress = false;
-    //         console.log("123456767");
-    //         show_pressButton();
-    //         await delay(2000);
-    //     } else {
-    //         return;
-    //     }
-    // }
-    // async function nogotrial(x) {
-    //     z = x - 2;
-    //     const iti = getRandomNogoNumber();
-    //     await delay(500); // 等待500毫秒
-    //     show_fixpoint(); // 播放
-    //     await delay(1000); // 等待播放結束
-    //     canpress = true; // 設定可以被按下
-    //     show_arrow(x - 2); // 播放
-    //     const diff = 500 - iti;
-    //     await delay(iti); // 等待播放結束
-    //     if (!canpress) {
-    //         console.log("fake!");
-    //         return;
-    //     }
-    //     z = x;
-    //     show_arrow(x); // 播放
-    //     await delay(diff);
-
-    //     if (!canpress) {
-    //         show_dontPressButton();
-    //         canpress = false; // 在這裡設置 canpress 為 false
-    //         await delay(2000);
-    //     }
-    // }
     async function nogotrial(x) {
         z = x - 2;
         const iti = getRandomNogoNumber();
@@ -416,7 +510,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     /* session */
     async function session(sArray) {
-        show_bg();
+        console.log("sessionstart");
+        const trial_num = sArray.length; // 將 trial_num 設置為 sArray 的長度
+        show_frame();
         updateButton();
         await delay(3000);
         for (let i = 0; i < trial_num; i++) {
@@ -427,6 +523,15 @@ document.addEventListener("DOMContentLoaded", function () {
                 await nogotrial(x);
             }
         }
+
+        console.log("sessionend");
+        clearAll();
+        killImg();
+    }
+
+    /* 清空場上一切 */
+    function clearAll() {
+        app.stage.removeChildren();
     }
 
     /*動畫*/
@@ -505,14 +610,32 @@ document.addEventListener("DOMContentLoaded", function () {
         init_Anime(img, 0.05);
     }
 
-    /*圖像*/
-    function show_bg() {
+    /*背景*/
+    async function show_bg(i) {
         if (background != null) {
             app.stage.removeChild(background);
         }
         // bg
-        let texture_bg = loader.resources.bg.texture;
+        let texture_bg = loader.resources.instruction1.texture;
+        if (i == 2) {
+            texture_bg = loader.resources.instruction2.texture;
+        }
         background = new PIXI.Sprite(texture_bg);
+        background.anchor.set(0.5); // 設置錨點在中心
+        const scale = app.renderer.height / background.height;
+        background.scale.set(scale);
+        background.position.set(app.screen.width / 2, app.screen.height / 2); // 設置位置在舞台中央
+        app.stage.addChildAt(background, 0); // 將背景放到最底層
+    }
+
+    /*圖像*/
+    function show_frame() {
+        if (background != null) {
+            app.stage.removeChild(background);
+        }
+        // bg
+        let texture_frame = loader.resources.frame.texture;
+        background = new PIXI.Sprite(texture_frame);
         background.anchor.set(0.5); // 設置錨點在中心
         background.scale.set(0.2);
         background.position.set(app.screen.width / 2, app.screen.height / 2); // 設置位置在舞台中央
@@ -535,7 +658,6 @@ document.addEventListener("DOMContentLoaded", function () {
         img.anchor.y = 0.5;
         app.stage.addChild(img);
     }
-
     function show_arrow_R_go() {
         // R
         let texture_right = loader.resources.arrow_R.texture;
@@ -577,5 +699,26 @@ document.addEventListener("DOMContentLoaded", function () {
             isPressing = true;
             show_arrow_L_nogo();
         }
+    }
+
+    // 函式來顯示文本
+    function show_text() {
+        // 文本內容
+        let textContent = "您的平均反應速度為\n" + "test";
+        "ms\n您的專屬代碼為\n" + randomString + "\n點選按鈕複製代碼至問卷表單";
+
+        // 創建PIXI.js Text對象
+        const text = new PIXI.Text(textContent, {
+            fontSize: 70,
+            fill: 0x000000,
+            wordWrap: true,
+            wordWrapWidth: 400, // 文本寬度，超過這個寬度會換行
+        });
+
+        // 設置文本的位置
+        text.position.set(50, 50);
+
+        // 將文本添加到PIXI場景
+        app.stage.addChild(text);
     }
 });
